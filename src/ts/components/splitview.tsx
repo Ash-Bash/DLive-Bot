6f87840d3e957d95f9e4d906b5fc118ed076e383
Ashley Chapman <Ash-Bash32@hotmail.co.uk>
2019-04-28 03:09:13 +0100

    menu items can now be selectable

--- src/ts/components/splitview.tsx
+++ src/ts/components/splitview.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+var classNames = require('classnames');
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab, faSpeakap } from '@fortawesome/free-brands-svg-icons';
@@ -23,7 +24,41 @@ const sidebarStyle = {
     margin: '0px'
 }
 
-export class SplitView extends React.Component {
+interface IState {
+    selectedIndex: number;
+}
+
+export class SplitView extends React.Component<any, IState> {
+
+    constructor(props: any){
+        super(props);
+
+        this.state = {
+            selectedIndex: 0
+        }
+        this.selectedListItem.bind(this);
+      }
+
+    selectedListItem(index: any) {
+        this.setState({selectedIndex: index});
+    }
+
+    itemIsActive(index: any) {
+        var isActive : boolean = false;
+        if (this.state.selectedIndex == index) {
+            isActive = true;
+        } else if (this.state.selectedIndex == index) {
+            isActive = true;
+        }  else if (this.state.selectedIndex == index) {
+            isActive = true;
+        }  else if (this.state.selectedIndex == index) {
+            isActive = true;
+        }  else if (this.state.selectedIndex == index) {
+            isActive = true;
+        } 
+        
+        return isActive;
+    }
 
     render() {
       return (
@@ -31,18 +66,20 @@ export class SplitView extends React.Component {
             <Row className="absolute fullHeightExcludeBar" style={ rowStyle }>
                 <Col xs="3" className="bg-sidebar" style={ sidebarStyle }>
                 <ListGroup flush>
-                    <ListGroupItem className="bg-sidebar" active tag="button" action><FontAwesomeIcon icon={ faHome } />   Home</ListGroupItem>
-                    <ListGroupItem className="bg-sidebar" tag="button" action><FontAwesomeIcon icon={ faCommentAlt } /> Chat <Badge className="right" pill>99+</Badge></ListGroupItem>
-                    <ListGroupItem className="bg-sidebar" tag="button" action><FontAwesomeIcon icon={ faUsers } />  Users <Badge className="right" pill>14</Badge></ListGroupItem>
-                    <ListGroupItem className="bg-sidebar" tag="button" action><FontAwesomeIcon icon={ faCode } />  Commands</ListGroupItem>
-                    <ListGroupItem className="bg-sidebar" tag="button" action><FontAwesomeIcon icon={ faCog } />  Settings</ListGroupItem>
+                    <ListGroupItem key="0" className={classNames({ 'bg-sidebar': true, 'active': this.itemIsActive(0) })} tag="button" action onClick={ () => this.selectedListItem(0) }><FontAwesomeIcon icon={ faHome } />   Home</ListGroupItem>
+                    <ListGroupItem key="1" className={classNames({ 'bg-sidebar': true, 'active': this.itemIsActive(1) })} tag="button" action onClick={ () => this.selectedListItem(1) }><FontAwesomeIcon icon={ faCommentAlt } /> Chat <Badge className="right" pill>99+</Badge></ListGroupItem>
+                    <ListGroupItem key="2" className={classNames({ 'bg-sidebar': true, 'active': this.itemIsActive(2) })} tag="button" action onClick={ () => this.selectedListItem(2) }><FontAwesomeIcon icon={ faUsers } />  Users <Badge className="right" pill>14</Badge></ListGroupItem>
+                    <ListGroupItem key="3" className={classNames({ 'bg-sidebar': true, 'active': this.itemIsActive(3) })} tag="button" action onClick={ () => this.selectedListItem(3) }><FontAwesomeIcon icon={ faCode } />  Commands</ListGroupItem>
+                    <ListGroupItem key="4" className={classNames({ 'bg-sidebar': true, 'active': this.itemIsActive(4) })} tag="button" action onClick={ () => this.selectedListItem(4) }><FontAwesomeIcon icon={ faCog } />  Settings</ListGroupItem>
                 </ListGroup>
                 <span></span>
                 <ListGroup flush>
                     
                 </ListGroup>
                 </Col>
-                <Col> Content View</Col>
+                <Col>
+
+                </Col>
             </Row>
         </Container>
       );
